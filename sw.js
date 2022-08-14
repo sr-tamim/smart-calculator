cc45e8ff4945905e06ed124ca5c080854e437122
SR Tamim <saifur.tamim@gmail.com>
2022-08-14 15:10:18 +0600

    google verification

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // service worker file
-const staticCacheName = 'calculator-v5.5.0';
+const staticCacheName = 'calculator-v5.5.5';
 const assets = [
     '/smart-calculator/',
     '/smart-calculator/index.html',
